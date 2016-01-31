c3f846989410c2d3bac117d8040236169d82524a
ryancheung <ryancheung.go@gmail.com>
2016-01-31 18:36:40 +0800

    Localize gossip menus

---
+++ updates/2016_01_30_locales_gossip_menu_option.sql
@@ -0,0 +1,86 @@
+UPDATE `gossip_menu_option` SET `option_text`='让我看看你出售的货物' WHERE `menu_id`=0 AND `id` IN (1,13);
+UPDATE `gossip_menu_option` SET `option_text`='我想快速旅行' WHERE `menu_id`=0 AND `id`=2;
+UPDATE `gossip_menu_option` SET `option_text`='训练我！' WHERE `menu_id`=0 AND `id`=3;
+UPDATE `gossip_menu_option` SET `option_text`='复活我' WHERE `menu_id`=0 AND `id` IN (4,5);
+UPDATE `gossip_menu_option` SET `option_text`='将这座旅店设为我的家' WHERE `menu_id`=0 AND `id`=6;
+UPDATE `gossip_menu_option` SET `option_text`='银行' WHERE `menu_id`=0 AND `id`=7;
+UPDATE `gossip_menu_option` SET `option_text`='我如何创建公会？' WHERE `menu_id`=0 AND `id`=8;
+UPDATE `gossip_menu_option` SET `option_text`='我想创建一个公会徽章。' WHERE `menu_id`=0 AND `id`=9;
+UPDATE `gossip_menu_option` SET `option_text`='我想进入战场。' WHERE `menu_id`=0 AND `id`=10;
+UPDATE `gossip_menu_option` SET `option_text`='拍卖行' WHERE `menu_id`=0 AND `id`=11;
+UPDATE `gossip_menu_option` SET `option_text`='我要寄存宠物' WHERE `menu_id`=0 AND `id`=12;
+UPDATE `gossip_menu_option` SET `option_text`='我希望遗忘自己的天赋' WHERE `menu_id`=0 AND `id`=14;
+UPDATE `gossip_menu_option` SET `option_text`='我想遗忘宠物的天赋' WHERE `menu_id`=0 AND `id`=15;
+
+UPDATE `gossip_menu_option` SET `option_text`='艾泽基尔说，你可能有某本书...' WHERE `menu_id`=8033 AND `id`=0;
+
+UPDATE `gossip_menu_option` SET `option_text`='带我去北地哨塔。' WHERE `menu_id`=7379 AND `id`=0;
+UPDATE `gossip_menu_option` SET `option_text`='带我去东墙哨塔。' WHERE `menu_id`=7379 AND `id`=1;
+UPDATE `gossip_menu_option` SET `option_text`='带我去皇冠哨塔。' WHERE `menu_id`=7379 AND `id`=2;
+
+UPDATE `gossip_menu_option` SET `option_text`='让我看看你出售的货物。' WHERE `menu_id`=9126 AND `id`=0;
+
+UPDATE `gossip_menu_option` SET `option_text`='我需要拦截黎明之刃的增援。' WHERE `menu_id`=9062 AND `id`=1;
+
+UPDATE `gossip_menu_option` SET `option_text`='让我看看你出售的货物。' WHERE `menu_id`=9087 AND `id`=0;
+
+UPDATE `gossip_menu_option` SET `option_text`='太阳井大门进度如何了？' WHERE `menu_id`=9286 AND `id`=2;
+
+UPDATE `gossip_menu_option` SET `option_text`='给我一个情况报告，队长。' WHERE `menu_id`=9285 AND `id`=0;
+
+UPDATE `gossip_menu_option` SET `option_text`='让我看看你出售的货物。' WHERE `menu_id`=9050 AND `id`=0;
+
+UPDATE `gossip_menu_option` SET `option_text`='说到行动，我已下令发动空袭。' WHERE `menu_id`=9062 AND `id`=0;
+
+UPDATE `gossip_menu_option` SET `option_text`='让我看看你出售的货物。' WHERE `menu_id`=9198 AND `id`=0;
+
+UPDATE `gossip_menu_option` SET `option_text`='<乘坐龙鹰去太阳之岛.>' WHERE `menu_id`=9143 AND `id`=0;
+
+UPDATE `gossip_menu_option` SET `option_text`='我上前线指挥官金斯顿轰炸任务。我需要一个狮鹫破坏者！' WHERE `menu_id`=7970 AND `id`=0;
+
+UPDATE `gossip_menu_option` SET `option_text`='当然！送我到天空卫队前哨站。' WHERE `menu_id`=8719 AND `id`=0;
+
+UPDATE `gossip_menu_option` SET `option_text`='送我去地狱岩床！' WHERE `menu_id`=8096 AND `id`=1;
+UPDATE `gossip_menu_option` SET `option_text`='送我去荣耀堡！' WHERE `menu_id`=8096 AND `id`=0;
+
+UPDATE `gossip_menu_option` SET `option_text`='告诉我可以飞去哪里？' WHERE `menu_id`=8095 AND `id`=0;
+
+UPDATE `gossip_menu_option` SET `option_text`='是的，我想到黑风谷去。' WHERE `menu_id`=8718 AND `id`=0;
+
+UPDATE `gossip_menu_option` SET `option_text`='我准备要飞下来到埃索达。' WHERE `menu_id`=7415 AND `id`=0;
+
+UPDATE `gossip_menu_option` SET `option_text`='我已经准备飞了！带我上去，龙！' WHERE `menu_id`=8229 AND `id`=0;
+
+UPDATE `gossip_menu_option` SET `option_text`='我早就准备好了。' WHERE `menu_id`=8082 AND `id`=0;
+
+UPDATE `gossip_menu_option` SET `option_text`='吉安娜告诉我要和你谈谈使用狮鹫调查阿尔卡兹岛。' WHERE `menu_id`=8782 AND `id`=0;
+
+UPDATE `gossip_menu_option` SET `option_text`='我准备好去敦霍尔德要塞了。' WHERE `menu_id`=7959 AND `id`=0;
+
+UPDATE `gossip_menu_option` SET `option_text`='听我说，人类。我不管你的困境，但你有我需要的东西。告诉我哪里可以找到血铸锭。' WHERE `menu_id`=7482 AND `id`=0;
+
+UPDATE `gossip_menu_option` SET `option_text`='我会找到你的男孩和密码，欧鲁诺克。' WHERE `menu_id`=8276 AND `id`=0;
+
+UPDATE `gossip_menu_option` SET `option_text`='所以，现在密码是什么？还有你的男孩呢？' WHERE `menu_id`=8277 AND `id`=0;
+
+UPDATE `gossip_menu_option` SET `option_text`='请继续。' WHERE `menu_id`=8278 AND `id`=0;
+
+UPDATE `gossip_menu_option` SET `option_text`='然而什么？欧鲁诺克？' WHERE `menu_id`=8279 AND `id`=0;
+
+UPDATE `gossip_menu_option` SET `option_text`='你是怎么知道这一切的？' WHERE `menu_id`=8280 AND `id`=0;
+
+UPDATE `gossip_menu_option` SET `option_text`='我要怎么才能找到密码？' WHERE `menu_id`=8281 AND `id`=0;
+
+UPDATE `gossip_menu_option` SET `option_text`='我准备好听你的故事了，欧鲁诺克。' WHERE `menu_id`=8259 AND `id`=1;
+UPDATE `gossip_menu_option` SET `option_text`='你都卖些什么，欧鲁诺克？' WHERE `menu_id`=8259 AND `id`=0;
+
+UPDATE `gossip_menu_option` SET `option_text`='发生什么事了？' WHERE `menu_id`=9287 AND `id`=0;
+
+UPDATE `gossip_menu_option` SET `option_text`='为什么要停下来？' WHERE `menu_id`=9288 AND `id`=0;
+
+UPDATE `gossip_menu_option` SET `option_text`='你很懂得欣赏。' WHERE `menu_id`=9289 AND `id`=0;
+
+UPDATE `gossip_menu_option` SET `option_text`='送我去破碎岗哨' WHERE `menu_id`=7970 AND `id`=1;
+UPDATE `gossip_menu_option` SET `option_text`='送我去破碎岗哨' WHERE `menu_id`=8100 AND `id`=0;
+
+UPDATE `gossip_menu_option` SET `option_text`='我准备好了，欧鲁诺克。让我们摧毁火焰之王森卢肯并释放元素！' WHERE `menu_id`=8350 AND `id`=0;
